fd9ea44db7c0d3f1d403a2338e34633770277485
Anvith S G <anvithsg2004@gmail.com>
2025-07-26 08:32:24 +0530

    Added Backend

---
+++ oauth-backend/src/test/java/com/oauth/oauth_backend/OauthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.oauth_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
